0cfcc976aeef8aafeee293fad6ad7955fb7826ee
rfredo <roeifredo@gmail.com>
2019-07-28 00:47:36 +0300

    full report 1

--- Free_Code_ML_Assignment.R
+++ Free_Code_ML_Assignment.R
@@ -3,6 +3,12 @@
 
 library(caret)
 library(corrplot)
+library(rattle)
+library(forecast)
+library(rpart)
+library(parallel)
+library(doParallel)
+library(xtable)
 
 url1 <- 'https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv'
 training <- basename(url1);
@@ -22,10 +28,10 @@ pmlTraining <- read.csv(training, na.strings = c("", "NA"))
 pmlTesting <- read.csv(testing, na.strings = c("", "NA"))
 
 #create validation set
-inTrain <- createDataPartition(y = pmlTraining$classe, p = .7, list - F)
+inTrain <- createDataPartition(y = pmlTraining$classe, p = .7, list = F)
 training <- pmlTraining[inTrain,]
-validation <- pmlTraining[-inTrain,]
-dim(training); dim(validation)
+testing <- pmlTraining[-inTrain,]
+dim(training); dim(testing)
 
 #matNA <- matrix(nrow = 1, ncol = dim(training)[2], dimnames = list("%NA", names(training)))
 #matNA[1,] <- as.numeric(matNA[1,])
@@ -38,16 +44,76 @@ dim(training); dim(validation)
 #Establish the NAs % for every columns for feature selection
 pmlNA <- sapply(training, FUN = function(x) mean(is.na(x)))
 training <- training[,pmlNA<0.2]
-validation <- validation[,pmlNA<0.2]
+testing <- testing[,pmlNA<0.2]
 
+#eliminating average observations
 training <- training[training$new_window == "no",]
-validation <-validation[validation$new_window == "no",]
+testing <-testing[testing$new_window == "no",]
 
+#remove feature with zero variance
 nzv <- nearZeroVar(training)
 training <- training[, -nzv]
-validation <- validation[, -nzv]
+testing <- testing[, -nzv]
 
-featurePlot(x = training[,-59], training$classe)
+#removing non-numeric first 5 columns
 
-corM <- cor(training[,-c(1:5,59)])
-corrplot.mixed(corM, order = "FPC", number.cx = .7)
+training <- training[,-c(1:5)]
+testing <- testing[,-c(1:5)]
+
+#featurePlot(x = training[,-59], training$classe)
+
+corM <- cor(training[,-54])
+corrplot(corM, order = "FPC", method = "color", type = "lower", tl.cex = .7)
+
+#Prediction ML models
+#cross validation
+
+set.seed(323)
+cluster <- makeCluster(detectCores() - 1)
+registerDoParallel(cluster)
+
+data_ctrl <- trainControl(method="cv", number = 5, allowParallel = T)
+
+#predicting with decision trees
+
+set.seed(323)
+modFittree <- train(classe ~ ., method = "rpart", data = training, trControl = data_ctrl)
+print(modFittree$finalModel)
+fancyRpartPlot(modFittree$finalModel)
+prpart <- predict(modFittree, testing)
+confrpart <- confusionMatrix(prpart, testing$classe)
+Accurtree <- conftree$overall[1]
+
+#predicting with random forest
+
+set.seed(323)
+modFitrf <- train(classe ~ ., method = "rf", data = training, trControl = data_ctrl)
+print(modFitrf$finalModel)
+prf <- predict(modFitrf, testing)
+confrf <- confusionMatrix(prf, testing$classe)
+Accurrf <- confrf$overall[1]
+
+
+
+#predicting eith GBM
+
+set.seed(323)
+modFitgbm <- train(classe ~ ., method = "gbm", data = training, verbose = F, trControl = data_ctrl)
+print(modFitgbm$finalModel)
+pgbm <- predict(modFitgbm, testing)
+confgbm <- confusionMatrix(pgbm, testing$classe)
+Accurgbm <- confgbm$overall[1]
+
+#predicting eith LDA
+
+set.seed(323)
+modFitlda <- train(classe ~ ., method = "lda", data = training, trControl = data_ctrl)
+print(modFitlda$finalModel)
+plda <- predict(modFitlda, testing)
+conflda <- confusionMatrix(plda, testing$classe)
+Accurlda <- conflda$overall[1]
+
+stopCluster(cluster)
+registerDoSEQ()
+
+table(Accurtree, Accurrf, Accurgbm, Accurlda)
